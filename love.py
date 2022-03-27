f3275ab13b3e58f4c3b624b9729c9401d65f1b6f
Shubham Varshney <shubham.varshney696@gmail.com>
2022-03-27 15:00:48 +0530

    Add files via upload

---
+++ love.py
@@ -0,0 +1,13 @@
+import imp
+from operator import imod
+from turtle import *
+color("red", "pink")
+begin_fill()
+left(50)
+forward(100)
+circle(40,180)
+left(260)
+circle(40,180)
+forward(100)
+end_fill()
+done()
